553da36bf2e00a08578ebeb710666fdb62d66d60
Fang Yongrui <1102649231@qq.com>
2024-03-06 16:29:18 +0800

    fix(tasklist): enable

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,5 +1,6 @@
 import { defineUserConfig } from "vuepress";
-import theme from "./theme.js";
+// import theme from "./theme.js";
+import {hopeTheme} from 'vuepress-theme-hope';
 
 export default defineUserConfig({
   base: "/",
@@ -8,8 +9,16 @@ export default defineUserConfig({
   title: "RCore",
   description: "vuepress-theme-hope 的文档演示",
 
-  theme,
+  //theme,
+  theme: hopeTheme({
+    plugins: {
+      mdEnhance: {
+        tasklist: true,
+      }
+    }
+  })
 
   // 和 PWA 一起启用
   // shouldPrefetch: false,
+
 });
