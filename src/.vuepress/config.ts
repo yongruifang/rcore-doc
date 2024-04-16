ae4569047075e12e723e3af57d8408ac82b0ec55
Fang Yongrui <1102649231@qq.com>
2024-04-16 23:43:55 +0800

    add header

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -8,6 +8,10 @@ export default defineUserConfig({
   title: "RCore",
   description: "vuepress-theme-hope 的文档演示",
 
+  head: [
+    ['meta', {name: 'google-site-verification', content: 'lOBM2JrQFkF_NCZ6FoQ4TP9j_OaIkwWZlnDIfgMqL7M'}]
+  ]
+
   theme,
   // 和 PWA 一起启用
   // shouldPrefetch: false,
