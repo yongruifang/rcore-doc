6956cc1133983d1277555f5875a15fdcedc48642
Fang Yongrui <1102649231@qq.com>
2024-04-17 14:00:28 +0800

    add(sougou,360)

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -11,6 +11,8 @@ export default defineUserConfig({
   head: [
     ['meta', {name: 'google-site-verification', content: 'lOBM2JrQFkF_NCZ6FoQ4TP9j_OaIkwWZlnDIfgMqL7M'}],
     ['meta', {name: 'baidu-site-verification', content: 'codeva-3OIj4Niri5'}],
+    ['meta', {name: 'sogou_site_verification', content: 'hIrYb0JCI5'}],
+    ['meta', {name: '360-site-verification', content: '04cd39b694dd9eecd11d5bcda1d3ed44'}],
   ],
 
   theme,
